98d4b87de091b3638c6515b4dab101817190f4d6
Afroz2109 <affu962k@gmail.com>
2025-07-24 11:22:22 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://tshirt-customizing-1.onrender.com',
   credentials: true,
 }));
 
